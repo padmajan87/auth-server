6f6477cc5e46bc09334e32e2ddd6ac75539356d7
Padmajan Venugopalan <padmajan87@gmail.com>
2021-12-30 20:21:57 +0530

    auth server

--- src/main/java/com/wipro/training3/authserver/entity/User.java
+++ src/main/java/com/wipro/training3/authserver/entity/User.java
@@ -9,12 +9,10 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
-import javax.persistence.JoinColumn;
 import javax.validation.constraints.Email;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class User {
